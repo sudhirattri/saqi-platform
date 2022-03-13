954a5988b93e2f1aafc306023dad1200f960fc1f
Sudhir Attri <sudhir18267@iiitd.ac.in>
2022-03-14 01:34:37 +0530

    scrape working cool

--- scraping-server/config/cpcb_config.py
+++ scraping-server/config/cpcb_config.py
@@ -7,7 +7,58 @@ locations = [
     }
 ]
 
-request_template={}
+request_template={
+    "draw": 1,
+    "columns": [
+        {
+            "data": 0,
+            "name": "",
+            "searchable": True,
+            "orderable": False,
+            "search": { 
+                "value": "",
+                "regex": False
+            }
+        }
+    ],
+    "order": [],
+    "start": 0,
+    "length": 10,
+    "search": {
+        "value": "",
+        "regex": False
+    },
+    "filtersToApply": {
+        "state": "Delhi",
+        "city": "Delhi",
+        "station": "site_1428",
+        "parameter_list": [
+            {
+                "id": 0,
+                "itemName": "PM2.5",
+                "itemValue": "parameter_193"
+            },
+            {
+                "id": 1,
+                "itemName": "PM10",
+                "itemValue": "parameter_215"
+            }
+        ],
+        "criteria": "15 Minute",
+        "reportFormat": "Tabular",
+        "fromDate": "11-03-2022 T00:00:00Z",
+        "toDate": "12-03-2022 T17:21:59Z",
+        "parameter": [
+            "parameter_193",
+            "parameter_215"
+        ],
+        "parameterNames": [
+            "PM2.5",
+            "PM10"
+        ]
+    },
+    "pagination": 1
+}
 
 job_template = {
     "status":"A",
--- scraping-server/services/cpcb.py
+++ scraping-server/services/cpcb.py
@@ -8,6 +8,11 @@ from config import cpcb_config
 import requests
 from datetime import datetime
 import copy
+import csv
+
+import os
+
+CAAQMS_URL = "https://app.cpcbccr.com/caaqms/fetch_table_data"
 
 def get_csv(data):
     pass
@@ -18,11 +23,22 @@ def run_job(job):
         print("Invalid current stage")
     
     if(job['stages'][current_stage]['name']=='acquisition'):
-        request_payload_dict = cpcb_config.request_template
+
+        stage_data = job['stages'][current_stage]["data"]
+
+        request_payload_dict = copy.deepcopy(cpcb_config.request_template)
+
+        request_payload_dict["filtersToApply"]["state"] = stage_data["state"]
+        request_payload_dict["filtersToApply"]["city"] = stage_data["city"]
+        request_payload_dict["filtersToApply"]["station"] = stage_data["station"]
+        request_payload_dict["filtersToApply"]["fromDate"] = stage_data["from_date"]
+        request_payload_dict["filtersToApply"]["toDate"] = stage_data["to_date"]
+
         payload_json = json.dumps(request_payload_dict) 
 
+
         request_payload_base64 = base64.b64encode(payload_json.encode("ascii"))
-        url = "https://app.cpcbccr.com/caaqms/fetch_table_data"
+
         headers = {
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
         'accept': 'application/json, text/plain, */*',
@@ -38,11 +54,11 @@ def run_job(job):
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'en-US,en;q=0.9,hi;q=0.8'
         }
-        response = requests.request("POST", url, headers=headers, data=request_payload_base64)
-        print(response.text)
+        # response = requests.request("POST", CAAQMS_URL, headers=headers, data=request_payload_base64)
+        # print(response.text)
+        response = json.load(open(os.path.join(os.path.dirname(__file__), './../example/api_response.json') , 'r'))
+        print(response)
 
-    elif(job['stages'][current_stage]['name']=='acquisition'):
-        pass
     elif(job['stages'][current_stage]['name']=='pre_process'):
         pass
     elif(job['stages'][current_stage]['name']=='mapping'):
@@ -61,18 +77,18 @@ def add_job():
 
     print("Adding cpcb jobs")
 
-    current_datetime = datetime.utcnow().strftime("%d-%m-%Y T%H:%M:%SZ")
-    last_hour_datetime = (datetime.utcnow() - timedelta(hours = 1)).strftime("%d-%m-%Y T%H:%M:%SZ")
-    
+    current_datetime = datetime.now().strftime("%d-%m-%Y T%H:%M:%SZ")
+    past_datetime = (datetime.now() - timedelta(hours = 4)).strftime("%d-%m-%Y T%H:%M:%SZ")
+
+    print(past_datetime)
     print(current_datetime)
-    print(last_hour_datetime)
 
     locations = cpcb_config.locations
     for loc in locations:
         job_template = copy.deepcopy(cpcb_config.job_template)
         job_template["last_run"] = str(datetime.now())
-        job_template['stages'][0]["data"]["from_date"] = current_datetime
-        job_template['stages'][0]["data"]["to_date"] = last_hour_datetime
+        job_template['stages'][0]["data"]["from_date"] = past_datetime
+        job_template['stages'][0]["data"]["to_date"] = current_datetime
         job_template['stages'][0]["data"]["state"] = loc["state"]
         job_template['stages'][0]["data"]["city"] = loc["city"]
         job_template['stages'][0]["data"]["station"] = loc["station"]
